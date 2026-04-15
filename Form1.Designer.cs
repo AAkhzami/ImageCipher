f42216b3136e43de5a0bb58dad4f0355cd601985
Just_Abdullrahman <abdullrahman.ak23@gmail.com>
2026-04-15 19:53:52 +0400

    Refactor image selection UI and file handling logic
    Renamed "button1" to "btnChooseImage" and updated all references for clarity. Added "lblFilePath" label to display the selected image path. Improved drag-and-drop logic to only accept .jpg and .png files, storing the path in a new private field `_FilePath`. Implemented file dialog selection with file type filtering and path display.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,37 +29,39 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnChooseImage = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.ofdChooseImage = new System.Windows.Forms.OpenFileDialog();
             this.label6 = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.AllowDrop = true;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(67)))), ((int)(((byte)(86)))));
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.lblFilePath);
+            this.panel1.Controls.Add(this.btnChooseImage);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
@@ -71,30 +73,31 @@
             this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
             this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
             // 
-            // button1
+            // btnChooseImage
             // 
-            this.button1.BackColor = System.Drawing.Color.MediumTurquoise;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(65, 374);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(288, 48);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Choose Image";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnChooseImage.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnChooseImage.FlatAppearance.BorderSize = 0;
+            this.btnChooseImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChooseImage.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseImage.ForeColor = System.Drawing.Color.White;
+            this.btnChooseImage.Location = new System.Drawing.Point(65, 374);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(288, 48);
+            this.btnChooseImage.TabIndex = 7;
+            this.btnChooseImage.Text = "Choose Image";
+            this.btnChooseImage.UseVisualStyleBackColor = false;
+            this.btnChooseImage.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(134, 234);
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(130, 225);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(151, 46);
+            this.label2.Size = new System.Drawing.Size(159, 46);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Drag only one file\r\nSupport JPG,PIN..";
+            this.label2.Text = "Drag only one file..\r\nSupport JPG,PIN..";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
@@ -107,6 +110,18 @@
             this.label1.Size = new System.Drawing.Size(349, 51);
             this.label1.TabIndex = 3;
             this.label1.Text = "Drop Image Here";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::ImageCipher.Properties.Resources.upload;
+            this.pictureBox1.InitialImage = global::ImageCipher.Properties.Resources.upload;
+            this.pictureBox1.Location = new System.Drawing.Point(159, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // panel2
             // 
@@ -123,6 +138,45 @@
             this.panel2.Size = new System.Drawing.Size(585, 450);
             this.panel2.TabIndex = 1;
             // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Location = new System.Drawing.Point(50, 291);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(504, 100);
+            this.panel4.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(261, 26);
+            this.button3.Margin = new System.Windows.Forms.Padding(35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 48);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(35, 26);
+            this.button2.Margin = new System.Windows.Forms.Padding(35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 48);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(1)))));
@@ -132,6 +186,39 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(504, 50);
             this.panel3.TabIndex = 10;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::ImageCipher.Properties.Resources.warning;
+            this.pictureBox3.Location = new System.Drawing.Point(116, 8);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(154, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Dont lose this password ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ImageCipher.Properties.Resources.visible;
+            this.pictureBox2.Location = new System.Drawing.Point(518, 136);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
             // 
             // label5
             // 
@@ -166,90 +253,6 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Encryption";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(154, 14);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(196, 22);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Dont lose this password ";
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = global::ImageCipher.Properties.Resources.warning;
-            this.pictureBox3.Location = new System.Drawing.Point(116, 8);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(36, 36);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 11;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::ImageCipher.Properties.Resources.visible;
-            this.pictureBox2.Location = new System.Drawing.Point(518, 136);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(36, 36);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 8;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::ImageCipher.Properties.Resources.upload;
-            this.pictureBox1.InitialImage = global::ImageCipher.Properties.Resources.upload;
-            this.pictureBox1.Location = new System.Drawing.Point(159, 42);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.button3);
-            this.panel4.Controls.Add(this.button2);
-            this.panel4.Location = new System.Drawing.Point(50, 291);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(504, 100);
-            this.panel4.TabIndex = 11;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Crimson;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(35, 26);
-            this.button2.Margin = new System.Windows.Forms.Padding(35);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(208, 48);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Encrypt";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(261, 26);
-            this.button3.Margin = new System.Windows.Forms.Padding(35);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(208, 48);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Decrypt";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
             // ofdChooseImage
             // 
             this.ofdChooseImage.FileName = "Choose Image";
@@ -265,6 +268,17 @@
             this.label6.Size = new System.Drawing.Size(318, 60);
             this.label6.TabIndex = 13;
             this.label6.Text = "Image Cipher";
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilePath.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblFilePath.Location = new System.Drawing.Point(20, 318);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(378, 23);
+            this.lblFilePath.TabIndex = 8;
+            this.lblFilePath.Text = "...Path...";
+            this.lblFilePath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmImageCipher
             // 
@@ -283,14 +297,14 @@
             this.Text = "Image Cipher";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -303,7 +317,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnChooseImage;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
@@ -316,6 +330,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.OpenFileDialog ofdChooseImage;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblFilePath;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace ImageCipher
 {
     public partial class frmImageCipher : Form
     {
+
+        string _FilePath = string.Empty;
         public frmImageCipher()
         {
             InitializeComponent();
@@ -20,21 +22,48 @@ namespace ImageCipher
         private void panel1_DragDrop(object sender, DragEventArgs e)
         {
             string[] FilePath = e.Data.GetData(DataFormats.FileDrop) as string[];
+            string extension = System.IO.Path.GetExtension(FilePath[0]).ToLower();
+            if (extension != ".jpg" && extension != ".png")
+            {
+                MessageBox.Show("File type is not supported");
+                return;
+            }
+
             if (FilePath.Length > 0)
             {
                 MessageBox.Show("File path : " + FilePath[0]);
+                _FilePath = FilePath[0];
+                lblFilePath.Text = _FilePath;
             }
-            else
-                MessageBox.Show("There are no path");
         }
 
         private void panel1_DragEnter(object sender, DragEventArgs e)
         {
             // to check if the data is in the form of file drop
+
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
                 e.Effect = DragDropEffects.Copy;
             else
                 e.Effect = DragDropEffects.None;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ofdChooseImage.Title = "Select Image to Encrypt";
+            ofdChooseImage.Filter = "Image Files (*.jpg; *.png)|*.jpg; *.png";
+            ofdChooseImage.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            ofdChooseImage.Multiselect = false;
+            ofdChooseImage.CheckFileExists = true;
+            ofdChooseImage.RestoreDirectory = true;
+
+            if(ofdChooseImage.ShowDialog() == DialogResult.OK)
+            {
+                _FilePath = ofdChooseImage.FileName;
+                MessageBox.Show("File path : " + _FilePath);
+                lblFilePath.Text = _FilePath;
+
+            }
+
         }
     }
 }
